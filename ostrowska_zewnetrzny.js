b53fa7afa89523abb17ce9e0d308a3514c49b7e4
JulkaOstrowska1 <julka.oset@gmail.com>
2025-09-21 19:16:32 +0200

    Update ostrowska_zewnetrzny.js

--- ostrowska_zewnetrzny.js
+++ ostrowska_zewnetrzny.js
@@ -1,2 +1,2 @@
 document.write("ostatnia modyfikacja strony".fontcolor("lime").bold().fontsize(5)+"<br>"); 
-document.write(document.lastModified.fontcolor("olive").bold().italic().fontsize(5)+"<br>"); 
+document.write(document.lastModified.strike().bold().fontcolor("olive").fontsize(5));
